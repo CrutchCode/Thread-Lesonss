7e233405f90548d406a8e185506713bea3924497
Serik <Serik@DESKTOP-7LVQVVL>
2020-06-01 22:38:00 +0300

    Добавьте файлы проекта.

---
+++ LessonsDownload/DownloadFile.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net;
+
+namespace LessonsDownload
+{
+    public class DownloadFile
+    {
+        public event Action<int> ProgressBar;
+        public event Action<int> ProgressBar1;
+        public event Action<int> ProgressBar2;
+        public void Download()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                ProgressBar(i);
+                Thread.Sleep(10);
+            }
+            string path = "https://toloka.to/photos/121204110646141807_f0_0.png";
+            string filename = "121204110646141807_f0_0.png";
+            WebClient client = new WebClient();
+            client.DownloadFile(path, filename);
+        }
+        public void Download1()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                ProgressBar1(i);
+                Thread.Sleep(10);
+            }
+            string path = "https://toloka.to/photos/180527042253642957_f0_0.jpg";
+            string filename = "180527042253642957_f0_0.jpg";
+            WebClient client = new WebClient();
+            client.DownloadFile(path, filename);
+        }
+        public void Download2()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                ProgressBar2(i);
+                Thread.Sleep(10);
+            }
+            string path = "https://simpsonsua.tv/photos/adventure-time/2.jpg";
+            string filename = "2.jpg";
+            WebClient client = new WebClient();
+            client.DownloadFile(path, filename);
+        }
+    }
+}
---
+++ ThreadLessonsWinForms/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using LessonsDownload;
+using System.Net;
+
+namespace ThreadLessonsWinForms
+{
+    public partial class Form1 : Form
+    {
+        DownloadFile downloadFile;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void ProgresBarChange(int progress)
+        {
+            Action action = () => progressBar1.Value = progress;
+            Invoke(action);
+        }
+        void ProgresBarChange1(int progress)
+        {
+            Action action = () => progressBar2.Value = progress;
+            Invoke(action);
+        }
+        void ProgresBarChange2(int progress)
+        { 
+            Action action = () => progressBar3.Value = progress;
+            Invoke(action);
+        }
+        //void DW()
+        //{
+        //    downloadFile = new DownloadFile();
+        //    downloadFile.ProgressBar += ProgresBarChange;
+        //    //downloadFile.ProgressBar1 += ProgresBarChange1;
+        //    //downloadFile.ProgressBar2 += ProgresBarChange2;
+
+        //    downloadFile.Download();
+        //    //    downloadFile.Download1();
+        //    //    downloadFile.Download2();
+        //}
+        private void button1_Click(object sender, EventArgs e)
+        {
+            downloadFile = new DownloadFile();
+            downloadFile.ProgressBar += ProgresBarChange;
+            downloadFile.ProgressBar1 += ProgresBarChange1;
+            downloadFile.ProgressBar2 += ProgresBarChange2;
+
+            button1.Enabled = false;
+            Thread thread = new Thread(downloadFile.Download);
+            thread.Start();
+            Thread thread1 = new Thread(downloadFile.Download1);
+            thread1.Start();
+            Thread thread2 = new Thread(downloadFile.Download2);
+            thread2.Start();
+            //button1.Enabled = true;
+        }
+    }
+}
